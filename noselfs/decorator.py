1d0fce2a04fa0c6f8c09fcd4019ffd03a7f47e42
Grant Stephens <grant@stephens.co.za>
2017-02-21 11:54:10 +0200

    Removed print

--- noselfs/decorator.py
+++ noselfs/decorator.py
@@ -1,12 +1,10 @@
 from functools import wraps
 from nose.plugins.attrib import attr
-from nose.tools import nottest
-from nose.tools import istest
+
 import os
 import pkg_resources
 import shlex
 import subprocess
-import traceback
 
 
 class lfstest(object):
@@ -40,7 +38,6 @@ class lfstest(object):
         @attr(*self.attrib, **self.named_attrib)
         @wraps(f)
         def wrapper(self, *args, **kwargs):
-            print 'run'
             if file_name_pass is None:
                 f(self, *args, **kwargs)
             else:
@@ -70,69 +67,3 @@ class lfstest(object):
                         'Tried LFS pull. Failed with: {}'.format(error)
                         )
         return file_name
-
-# @nottest
-# def lfstest(test_type, file_name=None, *args, **kwargs):
-#     """
-#     A wrapper to wrap all tests. It will also add any nose attributes that you
-#         give it.
-#     :param type: string- The type of test, be it unit, integration or model
-#     :param file_name: The file name needed as input to the test. This will
-#         get the file from LFS if it isn't available. Can also be a list of file
-#         names.
-#     :return file_name: The absolute path of the file_name given as input. If
-#         list is give a list will be returned.
-#     """
-#     named_attrib = kwargs
-#     named_attrib['type'] = test_type
-#     attrib = args
-#     # fff = file_name
-#
-#     def real_decorator(f, **kwargs):
-#
-#
-#         print file_name
-#         @attr(*attrib, **named_attrib)
-#         @wraps(f)
-#         def wrapper(self, *args, **kwargs):
-#             # if file_name is not None:
-#             #     if isinstance(file_name, list):
-#             #         output = []
-#             #         for file_n in file_name:
-#             #             output.append(_get_file(file_n))
-#             #         file_name = output
-#             #     else:
-#             #         file_name = _get_file(file_name, test_type)
-#             # print file_name
-#
-#             if file_name is None:
-#                 return f(self, *args, **kwargs)
-#             else:
-#                 return f(self, *args, unit_test_file=file_name, **kwargs)
-#         return wrapper
-#     return real_decorator
-#
-#
-# def _get_file(file_name, test_type):
-#     file_name_relative = os.path.join('data', test_type, file_name)
-#     file_name = pkg_resources.resource_filename(
-#         __name__, file_name_relative
-#         )
-#     print file_name
-#     if not os.path.isfile(file_name):
-#         raise IOError('Filename or Pointer not available.')
-#     elif os.path.getsize(file_name) < 300:
-#         with open(file_name) as open_file:
-#             check_words = [next(open_file).split(' ')[0] for x in xrange(3)]
-#         if all(word in check_words for word in ['version', 'size', 'oid']):
-#             process = subprocess.Popen(
-#                 shlex.split('git lfs pull --include="{}" --exclude=""'.format(
-#                     os.path.join('tests', file_name_relative))),
-#                 stdout=subprocess.PIPE
-#                 )
-#             output, error = process.communicate()
-#             if error is not None:
-#                 raise Exception(
-#                     'Tried LFS pull. Failed with: {}'.format(error)
-#                     )
-#     return file_name
