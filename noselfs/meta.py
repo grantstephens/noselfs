c6073e8a3508bc4424fe1ad65a3a9768c64cf6d5
Eugene Pretorius <eugene@healthqtech.com>
2017-03-22 13:53:40 +0200

    Version Bump

--- noselfs/meta.py
+++ noselfs/meta.py
@@ -1,7 +1,8 @@
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 """
 Changelog:
+0.0.9: Return the original function result through the decorator.
 0.0.8: Updated the testbase class to handle relative paths correctly
 0.0.7: Testbase remote fix
 0.0.6: Versioning weirdness
